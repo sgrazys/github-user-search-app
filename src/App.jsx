fc7ce120da7c90f6cc8103a33381a2a824bab607
Saulius Grazys <s.grazys@gmail.com>
2024-04-02 22:56:50 +0300

    app WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
 
 function App() {
-	return <div className='app'>devFinder. Coming Soon...</div>;
+	return (
+		<div className='app'>
+			<header>
+				<div className='top'>Logo + switch mode</div>
+				<div className='search'>Search</div>
+			</header>
+			<main>Main Info</main>
+			<footer>Made by</footer>
+		</div>
+	);
 }
 
 export default App;
